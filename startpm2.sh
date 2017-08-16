d6b9b269969ac797278d4ed102bc9fa99117c002
Chris Lofland <ekpyroticfrood@gmail.com>
2017-08-16 11:26:03 -0400

    first commit

---
+++ startpm2.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+export NVM_DIR="${HOME}/.nvm"
+[ -s "$NVM_DIR/nvm.sh"   ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+${HOME}/.nvm/versions/node/$(nvm current)/bin/pm2 start ${HOME}/RobotWebService/pm2Config.json
+
