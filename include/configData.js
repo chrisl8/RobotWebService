71e83385c118acdbcd1081be178d8bad83677e3b
Christen Lofland <ChristenLofland@gmail.com>
2021-05-29 20:00:40 -0500

    Remove unused config data import code.

--- include/configData.js
+++ include/configData.js
@@ -1,59 +1,6 @@
-// Load personal settings not included in git repo
-
 const fs = require('fs');
 
-const personalDataFile = `${process.env.HOME}/.robotWebService/config.json`;
-// For now just "FAKE" the default file.
-// TODO: Maybe later include it properly
-const defaultDataFile = `${process.env.HOME}/.robotWebService/config.json`;
-const configData = JSON.parse(fs.readFileSync(personalDataFile, 'utf8'));
-const defaultData = JSON.parse(fs.readFileSync(defaultDataFile, 'utf8'));
-
-// http://stackoverflow.com/a/130504
-function DumpObjectIndented(obj, indent) {
-  let result = '';
-  if (indent === null) {
-    // eslint-disable-next-line no-param-reassign
-    indent = '';
-  }
-
-  for (const property in obj) {
-    if (obj.hasOwnProperty(property)) {
-      let value = obj[property];
-      if (typeof value === 'string') value = `"${value}"`;
-      else if (typeof value === 'object') {
-        if (value instanceof Array) {
-          // Just let JS convert the Array to a string!
-          value = `[ ${value} ]`;
-        } else {
-          // Recursive dump
-          // (replace "  " by "\t" or something else if you prefer)
-          const od = DumpObjectIndented(value, `${indent}    `);
-          // If you like { on the same line as the key
-          value = `{\n${od}\n${indent}}`;
-          // If you prefer { and } to be aligned
-          // value = "\n" + indent + "{\n" + od + "\n" + indent + "}";
-        }
-      }
-      result += `${indent}"${property}" : ${value},\n`;
-    }
-  }
-  return result.replace(/,\n$/, '');
-}
-
-let updateNeeded = false;
-for (const prop in defaultData) {
-  if (configData[prop] === undefined) {
-    updateNeeded = true;
-    configData[prop] = defaultData[prop];
-  }
-}
-if (updateNeeded) {
-  const newFileOutputData = `{\n${DumpObjectIndented(configData, '    ')}\n}`;
-  // console.log(newFileOutputData);
-  fs.writeFile(personalDataFile, newFileOutputData);
-  console.log(`${personalDataFile} has been updated with new settings`);
-  console.log('Please check to see if you need to adjust them for your robot!');
-}
+const configDataFile = `${process.env.HOME}/.robotWebService/config.json`;
+const configData = JSON.parse(fs.readFileSync(configDataFile, 'utf8'));
 
 module.exports = configData;
